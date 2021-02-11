9ae5fcd39a5ae42c52509dbe1950ee29d6b15b41
Matrx007 <raintz1000@gmail.com>
2021-02-11 20:12:58 +0200

    Established the structure of the project

---
+++ src/GUI.java
@@ -0,0 +1,7 @@
+// Written by Rainis Randmaa
+
+// This class draws the GUI and talks with 'BackEnd' class (written by Gregor Suurvarik), which reacts to user
+// input and (if playing against the computer) takes turns as well.
+public class GUI {
+
+}
