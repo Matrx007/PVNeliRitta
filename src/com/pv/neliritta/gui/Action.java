c3046d46efc35d69baa78c30a6ac3c7faf3b47a4
Matrx007 <raintz1000@gmail.com>
2021-02-12 09:21:52 +0200

    Classes to use in GUI

---
+++ src/com/pv/neliritta/gui/Action.java
@@ -0,0 +1,5 @@
+package com.pv.neliritta.gui;
+
+public interface Action {
+    void run();
+}
---
+++ src/com/pv/neliritta/gui/Color.java
@@ -0,0 +1,19 @@
+package com.pv.neliritta.gui;
+
+public class Color {
+    public int r, g, b, a;
+
+    public Color(int r, int g, int b, int a) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+    }
+
+    public Color(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = 255;
+    }
+}
